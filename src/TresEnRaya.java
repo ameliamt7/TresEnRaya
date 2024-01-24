88093b36dbcad313a5fffd02faa7b21bbde04a35
ameliamt7 <ameliamartinezarcos@gmail.com>
2024-01-24 09:55:39 +0100

    Creo variables y método main

--- src/TresEnRaya.java
+++ src/TresEnRaya.java
@@ -7,5 +7,12 @@ public class TresEnRaya {
     public static void main (String [] args){
         Scanner input = new Scanner (System.in);
         int opcion=0;
+        do {
+            System.out.println("MENU");
+            System.out.println("1. Jugar");
+            System.out.println("2. Ver estatísticas");
+            System.out.println("0. Salir");
+
+        } while (opcion!=0);
     }
 }
