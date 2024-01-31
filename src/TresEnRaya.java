46c60624101e7919563afbfca071d4a2f1f1b3c8
ameliamt7 <ameliamartinezarcos@gmail.com>
2024-01-31 09:32:54 +0100

    2 diapositiva

--- src/TresEnRaya.java
+++ src/TresEnRaya.java
@@ -12,8 +12,30 @@ public class TresEnRaya {
             System.out.println("1. Jugar");
             System.out.println("2. Ver estatísticas");
             System.out.println("0. Salir");
-
+            try {
+                System.out.println("Seleccionar opcion(1-2) o 0 para salir");
+                opcion = 100;
+            } catch (InputMismatchExcepcion ime) {
+                opcion = 100;
+            }
+            input.nextLine();
+            switch (opcion) {
+                case 1:
+                    jugar(input);
+                    break;
+                case 2:
+                    System.out.println("Victorias:"+victorias);
+                    System.out.println("Derrotas:"+derrotas+"\n");
+                    break;
+                caso 0:
+                    System.out.println("Fin de programa\n");
+                    break;
+                default:
+                    System.out.println("Opción no válida \n");
+                    break;
+            }
         } while (opcion!=0);
         input.close();
+
     }
 }
